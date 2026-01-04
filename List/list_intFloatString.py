b77d257a1724e18660c2e0b720fb8cca7a65629e
Mayur Helkar <mayurhelkar@yahoo.com>
2026-01-04 19:41:10 +0530

    Add files via upload

---
+++ List/list_intFloatString.py
@@ -0,0 +1,36 @@
+print("Integer : variable list ::")
+List = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+print(List, end = ' ')
+
+print()
+
+print("List item values using For Loop :")
+for i in List:
+    print(i, end = ' ')
+
+print()
+print()
+
+print("String : variable list ::")
+List = [ 'January', 'February', 'March', 'April', "May", 'June', 'July', 'August', \
+        'September', 'October', 'November', 'December']
+print(List, end = ' ')
+
+print()
+
+print("List item values using For Loop :")
+for i in List:
+    print(i, end = ' ')
+
+print()
+print()
+
+print("String : variable list ::")
+List = [ 1.11, 2.22, 3.33, 4.44, 5.55, 6.66, 7.77, 8.88, 9.99 ]
+print(List, end = ' ')
+
+print()
+
+print("List item values using For Loop :")
+for i in List:
+    print(i, end = ' ')
