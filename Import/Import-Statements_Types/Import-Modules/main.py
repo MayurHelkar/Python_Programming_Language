15f4a7eb08ab41b67daed47e64ceb3514668236e
Mayur Helkar <mayurhelkar@yahoo.com>
2026-01-11 11:43:25 +0530

    Add files via upload

---
+++ Import/Import-Statements_Types/Import-Modules/main.py
@@ -0,0 +1,8 @@
+import calc
+
+print("Addition :", calc.addition(12, 5))
+print("Subtraction :", calc.subtraction(12, 5))
+print("Multiplication :", calc.multiplication(12, 5))
+print("Division Quotient :", calc.divisionQuotient(12, 5))
+print("Division Remainder :", calc.divisionRemainder(12, 5))
+print("Division :", calc.division_float(12, 5))
