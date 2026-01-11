cd463b975d48059a2f2febc6b6fd2bf63d402946
Mayur Helkar <mayurhelkar@yahoo.com>
2026-01-11 12:02:20 +0530

    Add files via upload

---
+++ Strings/String-Quotes_Multiple-Lines/strings_VariableUse_printFunction.py
@@ -0,0 +1,30 @@
+string_singleQuote = 'String using ' \
+'Single Quotes'
+print(string_singleQuote)
+
+print()
+
+string_doubleQuote = "String using " \
+"Double Quotes"
+print(string_doubleQuote)
+
+print()
+'''
+string_twice_singleQuote = ''String using Single Quotes'' # SyntaxError: unterminated triple-quoted string literal (detected at line 22)
+print(string_twice_doubleQuote) # "(" was not closed
+
+print()
+
+string_twice_doubleQuote = ""String using Double Quotes"" # SyntaxError: invalid syntax. Is this intended to be part of the string?
+print(string_twice_doubleQuote) # "(" was not closed
+
+print()
+'''
+
+print('''String using 
+      Thrice Single Quotes''')
+
+print()
+
+print("""String using 
+      Thrice Single Quotes""")
---
+++ Strings/String-Quotes_Multiple-Lines/strings_directUse_printFunction.py
@@ -0,0 +1,25 @@
+print('String using ' \
+'Single Quotes')
+
+print()
+
+print("String using " \
+"Double Quotes")
+
+print()
+'''
+print(''String using Single Quotes'') # "(" was not closed
+
+print()
+
+print(""String using Double Quotes"") # "(" was not closed
+
+print()
+'''
+print('''String using 
+      Thrice Single Quotes''')
+
+print()
+
+print("""String using 
+      Thrice Single Quotes""")
