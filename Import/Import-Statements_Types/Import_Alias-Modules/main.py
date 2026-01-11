56447a346c3144da4e0a466d2200a341fac91861
Mayur Helkar <mayurhelkar@yahoo.com>
2026-01-11 11:40:16 +0530

    Add files via upload

---
+++ Import/Import-Statements_Types/Import_Alias-Modules/main.py
@@ -0,0 +1,8 @@
+import calc as C
+
+print("Addition :", C.addition(12, 5))
+print("Subtraction :", C.subtraction(12, 5))
+print("Multiplication :", C.multiplication(12, 5))
+print("Division Quotient :", C.divisionQuotient(12, 5))
+print("Division Remainder :", C.divisionRemainder(12, 5))
+print("Division :", C.division_float(12, 5))
