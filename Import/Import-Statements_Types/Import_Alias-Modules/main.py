617b448eb62945f23ca64368f840bfe7d4016624
Mayur Helkar <mayurhelkar@yahoo.com>
2026-01-18 22:39:42 +0530

    Delete Import directory

--- Import/Import-Statements_Types/Import_Alias-Modules/main.py
+++
@@ -1,8 +0,0 @@
-import calc as C
-
-print("Addition :", C.addition(12, 5))
-print("Subtraction :", C.subtraction(12, 5))
-print("Multiplication :", C.multiplication(12, 5))
-print("Division Quotient :", C.divisionQuotient(12, 5))
-print("Division Remainder :", C.divisionRemainder(12, 5))
-print("Division :", C.division_float(12, 5))
--- Import/Import-Statements_Types/Import_All-Methods/main.py
+++
@@ -1,8 +0,0 @@
-from calc import *
-
-print("Addition :", addition(12, 5))
-print("Subtraction :", subtraction(12, 5))
-print("Multiplication :", multiplication(12, 5))
-print("Division Quotient :", divisionQuotient(12, 5))
-print("Division Remainder :", divisionRemainder(12, 5))
-print("Division :", division_float(12, 5))
