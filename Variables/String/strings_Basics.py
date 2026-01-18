4497a4b18c22483ba7a7926eb6386f420100ee72
Mayur Helkar <mayurhelkar@yahoo.com>
2026-01-18 22:51:01 +0530

    Add files via upload

---
+++ Variables/String/strings_Basics.py
@@ -0,0 +1,25 @@
+# Strings
+
+print("Strings Basics :")
+
+print("Character with Single Quotes :")
+character_singleQuote = [ 'a', 'b', 'c', 'd' ]
+print(character_singleQuote)
+
+print()
+
+print("Character with Single Quotes :")
+character_doubleQuote = [ "A", "B", "C", "D" ]
+print(character_doubleQuote)
+
+print()
+
+print('String with Single Quote')
+string_singleQuote = 'Hello World'
+print(string_singleQuote)
+
+print()
+
+print('String with Double Quote')
+string_doubleQuote = "Hello World"
+print(string_doubleQuote)
