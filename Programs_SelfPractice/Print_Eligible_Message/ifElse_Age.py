7ec296003a2325e11985c39419e0a02031f27825
Mayur Helkar <mayurhelkar@yahoo.com>
2026-02-09 12:18:38 +0530

    Add files via upload

---
+++ Programs_SelfPractice/Print_Eligible_Message/ifElse_Age.py
@@ -0,0 +1,11 @@
+age = 15
+
+if age >= 18:
+    message = "Eligible"
+else:
+    message = "Not eligible"
+
+print("message : ", message)
+
+Message = "Eligible" if age >=18 else "Not eligible"
+print("Message : ", Message)
---
+++ Programs_SelfPractice/Print_Eligible_Message/ifElse_Age_oneLine.py
@@ -0,0 +1,5 @@
+age = 16
+
+message = "Eligible" if age >=18 else "Not eligible"
+
+print(message)
